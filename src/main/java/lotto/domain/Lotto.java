3d0832afe9700f7947f7bf6152f3a5d877b969d7
jlhyunii <jhpark4692@naver.com>
2025-11-03 23:05:50 +0900

    feat: 로또 발행 시 번호 검증 로직 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -14,6 +16,20 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+        }
+
+        boolean invalidRange = numbers.stream().anyMatch(n -> n < 1 || n > 45);
+        if (invalidRange) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이여야 합니다.");
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 
     @Override
